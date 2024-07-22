88361d79649f3109ec4c686e9a436297c9ef4bd5
vitukjkk <vittorcolom@gmail.com>
2024-07-22 20:59:36 -0300

    :)

---
+++ userdata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas_do_Saber
+{
+    internal class userdata
+    {
+        protected int id;
+        protected string name;
+        protected string username;
+        protected string senha;
+        protected string email;
+    }
+}
